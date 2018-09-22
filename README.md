74558dcc69ac567cfb979c281bffb54fb39ee561
lideo <1573567+lideo@users.noreply.github.com>
2018-09-22 23:43:53 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+#Description
+
+A very simple script that optimizes large jpg and png images in a given folder. 
+
+It uses the [Sharp](https://github.com/lovell/sharp) Node module for the optimization.
+
+##Installation
+
+    npm install
+
+##Usage
+
+Save the original images in the `/src/img` folder.
+
+    npm run minify
+    
+The optimized files will be copied to a folder called `/img`. 
+
+It also makes a copy of the images that don't need optimization.
+
+    
---
+++ image-optimization.js
@@ -0,0 +1,91 @@
+const sharp = require('sharp');
+const fse = require('fs-extra');
+const walk = require('fs-walk');
+const mkdirp = require('mkdirp');
+const sizeOf = require('image-size');
+
+// Sharp options
+const options = {
+    entry: 'src/img',
+    jpeg : {
+        maxWidth: 1920,
+        maxHeight: 1080,
+        quality: 80,
+        progressive: true,
+    },
+    png: {
+        compressionLevel: 9,
+        progressive: false,
+    }
+}
+
+const isLargeEnough = (size) => size >= 50000; // Bytes
+const isJpeg = (type) => type == 'jpg';
+const isPng = (type) => type == 'png';
+const isPortraitOrSquare = (fileData) => fileData.width <= fileData.height;
+
+walk.walk(options.entry, function(basedir, filename, stat, next) {
+    if (stat.isFile()) {
+
+        const destinationDir = basedir.replace('src/', '');
+        const filePath = `${basedir}/${filename}`;
+
+        if (isLargeEnough(stat.size)) {
+
+            const fileData = sizeOf(filePath);
+
+            let resizeWidth, resizeHeight;
+            if (isPortraitOrSquare(fileData)) {
+                resizeHeight = options.jpeg.maxWidth;
+            } else {
+                resizeWidth = options.jpeg.maxWidth;
+            }
+
+            if (isJpeg(fileData.type)) {
+
+                (async () => {
+                    await mkdirp(destinationDir);
+
+                    await sharp(filePath)
+                        .resize(resizeWidth, resizeHeight)
+                        .max()
+                        .jpeg({
+                            quality: options.jpeg.quality,
+                            progressive: options.jpeg.progressive,
+                        })
+                        .toFile(`${destinationDir}/${filename}`)
+                        .then(data => console.log('+++++ Generating jpg:', filePath, `${destinationDir}/${filename}`))
+                        .catch(err => console.error(err))
+                })();
+
+            } else if (isPng(fileData.type)) {
+                (async () => {
+                    await mkdirp(destinationDir);
+
+                    await sharp(filePath)
+                        .png({
+                            progressive: options.png.progressive,
+                            compressionLevel: options.png.compressionLevel,
+                        })
+                        .toFile(`${destinationDir}/${filename}`)
+                        .then(data => console.log('+++++ Generating png:', filePath, `${destinationDir}/${filename}`))
+                        .catch(err => console.error(err))
+                })();
+            }
+        } else {
+
+            fse.copy(filePath, `${destinationDir}/${filename}`)
+                .then(() => console.log('##### Copying intact file:', filePath, `${destinationDir}/${filename}`))
+                .catch(err => console.error(err))
+
+        }
+
+    } else if (stat.isDirectory()) {
+        console.log('directory', basedir, filename);
+    }
+
+    next();
+
+}, function(err) {
+    if (err) console.log(err);
+});
